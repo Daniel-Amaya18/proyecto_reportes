998b2263a94916a760ae8822c811db885344d821
Daniel-Amaya18 <juandaamaya18@gmail.com>
2023-03-28 23:23:59 -0500

    Modulo reportes con requests

--- servicio_reportes/reportes/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Torneo
-
-class torneo_serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Torneo
-        fields = ['ide','person_1','person_2','person_3'] # Modificar atributos
